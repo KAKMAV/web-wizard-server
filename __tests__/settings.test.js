42f31a567a24f083a3570d0a8aae5721048bd2aa
KARA PEDERSEN <KJPED74@GMAIL.COM>
2021-07-08 12:24:14 -0700

    get all setting test passing

--- __tests__/settings.test.js
+++ __tests__/settings.test.js
@@ -33,4 +33,42 @@ describe('setting routes', () => {
       elementSize: '12',
     });
   });
+
+  it('finds all settngs via GET', async () => {
+    const calvin = await Setting.insert({
+      userId: 'calvin',
+      pageUrl: 'calvin.com',
+      backgroundColor: 'green',
+      elementColor: 'blue',
+      fontFamily: 'cursive',
+      fontSize: '14',
+      elementSize: '45'
+    });
+
+    const scarlet = await Setting.insert({
+      userId: 'scarlet',
+      pageUrl: 'scarlet.com',
+      backgroundColor: 'pink',
+      elementColor: 'purple',
+      fontFamily: 'times new roman',
+      fontSize: '12',
+      elementSize: '30'
+
+    });
+
+    const matt = await Setting.insert({
+      userId: 'matt',
+      pageUrl: 'matt.com',
+      backgroundColor: 'turquoise',
+      elementColor: 'yellow',
+      fontFamily: 'sans sarif',
+      fontSize: '11',
+      elementSize: '75'
+    });
+
+    const res = await request(app)
+      .get('/api/v1/settings');
+
+    expect(res.body).toEqual([calvin, scarlet, matt]);
+  });
 });
